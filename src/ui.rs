9e66f1ae8b6c752e411898bc99c04c0ff8bf2185
BigWingBeat <bigwingbeat@siliconwings.net>
2025-10-31 18:25:41 +0000

    Use the macros

--- src/ui.rs
+++ src/ui.rs
@@ -8,7 +8,7 @@ use bevy::{
     input_focus::tab_navigation::TabGroup,
     prelude::*,
     ui::Checked,
-    ui_widgets::{RadioGroup, ValueChange},
+    ui_widgets::{RadioGroup, ValueChange, observe},
 };
 
 use crate::configuration::{ActiveSimulation, ActiveTimesteps, respawn};
@@ -26,33 +26,29 @@ pub fn plugin(app: &mut App) {
 }
 
 fn setup(mut commands: Commands) {
-    commands
-        .spawn((
-            Node {
-                position_type: PositionType::Absolute,
-                left: Val::Px(12.0),
-                top: Val::Px(120.0),
-                padding: UiRect::all(Val::Px(12.0)),
-                border: UiRect::all(Val::Px(2.0)),
-                flex_direction: FlexDirection::Column,
-                row_gap: Val::Px(12.0),
-                ..default()
-            },
-            BackgroundColor(feathers::palette::GRAY_1),
-            BorderColor::all(feathers::palette::WARM_GRAY_1),
-            BorderRadius::all(Val::Px(4.0)),
-            TabGroup::default(),
-        ))
-        .with_children(|children| {
-            children
-                .spawn((
-                    Node {
-                        flex_direction: FlexDirection::Column,
-                        ..default()
-                    },
-                    RadioGroup,
-                ))
-                .observe(
+    commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            left: Val::Px(12.0),
+            top: Val::Px(120.0),
+            padding: UiRect::all(Val::Px(12.0)),
+            border: UiRect::all(Val::Px(2.0)),
+            flex_direction: FlexDirection::Column,
+            row_gap: Val::Px(12.0),
+            ..default()
+        },
+        BackgroundColor(feathers::palette::GRAY_1),
+        BorderColor::all(feathers::palette::WARM_GRAY_1),
+        BorderRadius::all(Val::Px(4.0)),
+        TabGroup::default(),
+        children![
+            (
+                Node {
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                RadioGroup,
+                observe(
                     |on: On<ValueChange<Entity>>,
                      radios: Query<(Entity, &SimulationRadioButton)>,
                      mut active_simulation: ResMut<ActiveSimulation>,
@@ -66,115 +62,101 @@ fn setup(mut commands: Commands) {
                                 commands.entity(entity).remove::<Checked>();
                             }
                         }
-                    },
-                )
-                .with_children(|radios| {
-                    radios.spawn(Text::new("Switch active simulation:"));
-
-                    radios
-                        .spawn(radio(
-                            (
-                                Checked,
-                                SimulationRadioButton(ActiveSimulation::LorenzAttractor),
-                            ),
-                            (),
-                        ))
-                        .with_child(Text::new("Lorenz Attractor"));
-
-                    radios
-                        .spawn(radio(
-                            SimulationRadioButton(ActiveSimulation::MouseCursor),
-                            (),
-                        ))
-                        .with_child(Text::new("Mouse Cursor"));
-
-                    radios
-                        .spawn(radio(
-                            SimulationRadioButton(ActiveSimulation::MovingBox),
-                            (),
-                        ))
-                        .with_child(Text::new("Moving Box"));
-                });
-
-            children
-                .spawn((Node {
+                    }
+                ),
+                children![
+                    Text::new("Switch active simulation:"),
+                    (radio(
+                        (
+                            Checked,
+                            SimulationRadioButton(ActiveSimulation::LorenzAttractor)
+                        ),
+                        Spawn(Text::new("Lorenz Attractor"))
+                    )),
+                    (radio(
+                        SimulationRadioButton(ActiveSimulation::MouseCursor),
+                        Spawn(Text::new("Mouse Cursor"))
+                    )),
+                    (radio(
+                        SimulationRadioButton(ActiveSimulation::MovingBox),
+                        Spawn(Text::new("Moving Box"))
+                    ))
+                ]
+            ),
+            (
+                Node {
                     flex_direction: FlexDirection::Column,
                     ..default()
-                },))
-                .with_children(
-                    |toggles: &mut bevy::ecs::relationship::RelatedSpawnerCommands<'_, ChildOf>| {
-                        toggles.spawn(Text::new("Timestep toggles:"));
-
-                        toggles
-                            .spawn(checkbox((), ()))
-                            .with_child(Text::new("No Delta Time"))
-                            .observe(
-                                |on: On<ValueChange<bool>>,
-                                 mut active_timesteps: ResMut<ActiveTimesteps>,
-                                 mut commands: Commands| {
-                                    active_timesteps.toggle(ActiveTimesteps::NO_DELTA);
-                                    if on.value {
-                                        commands.entity(on.source).insert(Checked);
-                                    } else {
-                                        commands.entity(on.source).remove::<Checked>();
-                                    }
-                                    commands.run_system_cached(respawn);
-                                },
-                            );
-
-                        toggles
-                            .spawn(checkbox((), ()))
-                            .with_child(Text::new("Variable Delta Time"))
-                            .observe(
-                                |on: On<ValueChange<bool>>,
-                                 mut active_timesteps: ResMut<ActiveTimesteps>,
-                                 mut commands: Commands| {
-                                    active_timesteps.toggle(ActiveTimesteps::VARIABLE_DELTA);
-                                    if on.value {
-                                        commands.entity(on.source).insert(Checked);
-                                    } else {
-                                        commands.entity(on.source).remove::<Checked>();
-                                    }
-                                    commands.run_system_cached(respawn);
-                                },
-                            );
-
-                        toggles
-                            .spawn(checkbox(Checked, ()))
-                            .with_child(Text::new("Semi-Fixed Timestep"))
-                            .observe(
-                                |on: On<ValueChange<bool>>,
-                                 mut active_timesteps: ResMut<ActiveTimesteps>,
-                                 mut commands: Commands| {
-                                    active_timesteps.toggle(ActiveTimesteps::SEMI_FIXED);
-                                    if on.value {
-                                        commands.entity(on.source).insert(Checked);
-                                    } else {
-                                        commands.entity(on.source).remove::<Checked>();
-                                    }
-                                    commands.run_system_cached(respawn);
-                                },
-                            );
-
-                        toggles
-                            .spawn(checkbox((), ()))
-                            .with_child(Text::new("Fixed Timestep"))
-                            .observe(
-                                |on: On<ValueChange<bool>>,
-                                 mut active_timesteps: ResMut<ActiveTimesteps>,
-                                 mut commands: Commands| {
-                                    active_timesteps.toggle(ActiveTimesteps::FIXED);
-                                    if on.value {
-                                        commands.entity(on.source).insert(Checked);
-                                    } else {
-                                        commands.entity(on.source).remove::<Checked>();
-                                    }
-                                    commands.run_system_cached(respawn);
-                                },
-                            );
-                    },
-                );
-
-            children.spawn((Text::default(), SimulationDescription));
-        });
+                },
+                children![
+                    Text::new("Timestep Toggles:"),
+                    checkbox(
+                        observe(
+                            |on: On<ValueChange<bool>>,
+                             mut active_timesteps: ResMut<ActiveTimesteps>,
+                             mut commands: Commands| {
+                                active_timesteps.toggle(ActiveTimesteps::NO_DELTA);
+                                if on.value {
+                                    commands.entity(on.source).insert(Checked);
+                                } else {
+                                    commands.entity(on.source).remove::<Checked>();
+                                }
+                                commands.run_system_cached(respawn);
+                            }
+                        ),
+                        Spawn(Text::new("No Delta Time"))
+                    ),
+                    checkbox(
+                        observe(
+                            |on: On<ValueChange<bool>>,
+                             mut active_timesteps: ResMut<ActiveTimesteps>,
+                             mut commands: Commands| {
+                                active_timesteps.toggle(ActiveTimesteps::VARIABLE_DELTA);
+                                if on.value {
+                                    commands.entity(on.source).insert(Checked);
+                                } else {
+                                    commands.entity(on.source).remove::<Checked>();
+                                }
+                                commands.run_system_cached(respawn);
+                            }
+                        ),
+                        Spawn(Text::new("Variable Delta Time"))
+                    ),
+                    checkbox(
+                        observe(
+                            |on: On<ValueChange<bool>>,
+                             mut active_timesteps: ResMut<ActiveTimesteps>,
+                             mut commands: Commands| {
+                                active_timesteps.toggle(ActiveTimesteps::SEMI_FIXED);
+                                if on.value {
+                                    commands.entity(on.source).insert(Checked);
+                                } else {
+                                    commands.entity(on.source).remove::<Checked>();
+                                }
+                                commands.run_system_cached(respawn);
+                            }
+                        ),
+                        Spawn(Text::new("Semi-Fixed Timestep"))
+                    ),
+                    checkbox(
+                        observe(
+                            |on: On<ValueChange<bool>>,
+                             mut active_timesteps: ResMut<ActiveTimesteps>,
+                             mut commands: Commands| {
+                                active_timesteps.toggle(ActiveTimesteps::FIXED);
+                                if on.value {
+                                    commands.entity(on.source).insert(Checked);
+                                } else {
+                                    commands.entity(on.source).remove::<Checked>();
+                                }
+                                commands.run_system_cached(respawn);
+                            }
+                        ),
+                        Spawn(Text::new("Fixed Timestep"))
+                    ),
+                ]
+            ),
+            (Text::default(), SimulationDescription)
+        ],
+    ));
 }
